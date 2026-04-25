7a4be765d032b149e4d3e4ef5e2f32e99684794c
Yerson Fabian Garzon Rodriguez <141519552+YersonRodriguez2005@users.noreply.github.com>
2026-04-25 18:45:56 -0500

    Backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
