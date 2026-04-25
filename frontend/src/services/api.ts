4493ef59f11913f8fb3f94acc53fd51084109779
Yerson Fabian Garzon Rodriguez <141519552+YersonRodriguez2005@users.noreply.github.com>
2026-04-24 20:44:17 -0500

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Creamos la instancia base apuntando a nuestro servidor local
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://barbersync-v2lb.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
